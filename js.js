ea9b6e300b54454c580957fb1de6e7159c29689d
Mobinul Islam Mahi <71390423+Mobinulislammahi@users.noreply.github.com>
2021-09-17 21:27:04 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,18 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusslides(n) {
+  showSlides(slideIndex += n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("sliderimg");
+  if (n > slides.length) {slideIndex = 1}    
+  else if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block"; 
+
+}
+	
